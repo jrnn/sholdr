00bb14046c0196d76c90c63cfd9dbd4a0f182fbe
Juurinen Juho T <juho.juurinen@gmail.com>
2018-05-28 14:21:29 +0300

    view for looking at and updating individual shareholder

--- app/views/shareholder.py
+++ app/views/shareholder.py
@@ -26,12 +26,12 @@ bp = Blueprint(
 @bp.route("/", methods = ("GET",))
 def list_all():
     """
-    Because shareholder has two subclasses and the data needed here is in those
-    subclasses, use with_polymorphic() to make an "eager" JOIN query, so that
+    Because shareholder has two subclasses and the data needed here is on sub-
+    class level, use with_polymorphic() to make an "eager" JOIN query, so that
     needed data is all loaded up-front at once. This is to avoid a sequence of
-    pointless per-entity queries afterwards (i.e. the N+1 problem).
+    pointless per-entity queries (i.e. the N+1 problem).
 
-    TO-DO : Replace ORM default with a manual, lightweight query?
+    TO-DO : Replace ORM default with a manual, more lightweight query?
     """
     shareholders = db.session.query(
         with_polymorphic(
@@ -46,68 +46,112 @@ def list_all():
 
 @bp.route("/<id>", methods = ("GET",))
 def view_one(id):
-    return "render shareholder/form.html with prefilled form depending on type"
+    """
+    Find one shareholder by primary key, then query for the correct subclass
+    entity by type, and prefill corresponding form with its data. The same form
+    template is used as when creating new shareholders, but the form action on
+    submit points at the 'update' route, singled by id.
+    """
+    s = Shareholder.query.get_or_404(id)
+
+    if s.type == "natural_person":
+        s = NaturalPerson.query.get(id)
+        f = NaturalPersonForm(obj = s)
+    else:
+        s = JuridicalPerson.query.get(id)
+        f = JuridicalPersonForm(obj = s)
+
+    return render_template(
+        "shareholder/form.html",
+        form = f,
+        form_action = url_for("shareholder.create") + id
+    )
 
 @bp.route("/new", methods = ("GET",))
 def form():
     """
-    Select form depending on shareholder type, which is given as query param.
+    Shareholder type must be given as query parameter, based on which the
+    correct form is passed to Jinja. Form action on submit is set to point at
+    'create' route.
     """
     type = request.args.get("type")
 
     if type == "natural":
-        return render_template(
-            "shareholder/form.html",
-            form = NaturalPersonForm()
-        )
+        f = NaturalPersonForm()
     elif type == "juridical":
-        return render_template(
-            "shareholder/form.html",
-            form = JuridicalPersonForm()
-        )
+        f = JuridicalPersonForm()
     else:
         ## TO-DO : error message
         return redirect(url_for("shareholder.list_all"))
 
+    return render_template(
+        "shareholder/form.html",
+        form = f,
+        form_action = url_for("shareholder.create")
+    )
+
 @bp.route("/", methods = ("POST",))
 def create():
+    """
+    Convert html form into corresponding WTForm and run validation. If errors,
+    throw back to form view. Otherwise populate a new shareholder object with
+    form data and save into db.
+    """
     type = request.form.get("type")
     assert type == "natural" or type == "juridical"
 
     if type == "natural":
         f = NaturalPersonForm(request.form)
         s = NaturalPerson()
-
-        s.first_name = f.first_name.data.strip()
-        s.last_name = f.last_name.data.strip()
-        s.nin = f.nin.data.strip()
-        s.nationality = f.nationality.data.strip()
-
     else:
         f = JuridicalPersonForm(request.form)
         s = JuridicalPerson()
-
-        s.name = f.name.data.strip()
-        s.business_id = f.business_id.data.strip()
-        s.contact_person = f.contact_person.data.strip()
 
     if not f.validate():
         # TO-DO : error message
         return render_template(
             "shareholder/form.html",
-            form = f
+            form = f,
+            form_action = url_for("shareholder.create")
         )
 
-    s.email = f.email.data.strip()
-    s.pw_hash = f.password.data.strip()
-    s.street = f.street.data.strip()
-    s.street_ext = f.street_ext.data.strip()
-    s.zip_code = f.zip_code.data.strip()
-    s.city = f.city.data.strip()
-    s.country = f.country.data.strip()
+    f.populate_obj(s)
+    s.pw_hash = "kuha_on_varaani"  ## TEMPORARY BULLSHIT
 
-    db.session().add(s)
-    db.session().commit()
+    db.session.add(s)
+    db.session.commit()
+
+    ## TO-DO : success message
+    return redirect(url_for("shareholder.list_all"))
+
+@bp.route("/<id>", methods = ("POST",))
+def update(id):
+    """
+    Very similar to create() above, but instead of adding new shareholder, fetch
+    existing one from db, overwrite it with form data, and save changes.
+
+    TO-DO : maybe create() and update() should be integrated into one method?
+    """
+    type = request.form.get("type")
+    assert type == "natural" or type == "juridical"
+
+    if type == "natural":
+        s = NaturalPerson.query.get_or_404(id)
+        f = NaturalPersonForm(request.form)
+    else:
+        s = JuridicalPerson.query.get_or_404(id)
+        f = JuridicalPersonForm(request.form)
+
+    if not f.validate():
+        # TO-DO : error message
+        return render_template(
+            "shareholder/form.html",
+            form = f,
+            form_action = url_for("shareholder.create") + id
+        )
+
+    f.populate_obj(s)
+    db.session.commit()
 
     ## TO-DO : success message
     return redirect(url_for("shareholder.list_all"))
